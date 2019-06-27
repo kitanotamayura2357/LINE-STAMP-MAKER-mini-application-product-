4b7dd62fc3f5a25c2289e2b4581775eeb944a704
kitanotamayura2357 <kitanotamayura2357@gmail.com>
2019-06-27 23:03:21 +0900

    Make save button

--- line-stamp-maker/line-stamp-maker/ViewController.swift
+++ line-stamp-maker/line-stamp-maker/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        
-        TitleLabel.text = "Stamp maker"
+        TitleLabel.text = "Line stamp maker"
 
     }
     
